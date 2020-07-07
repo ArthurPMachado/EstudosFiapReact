e23462575f5c28d4b950c85fcecf3e1a30e3cf81
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-07-06 22:20:18 -0300

    Feat: request with axios

--- src/components/PrimeiroComponente.jsx
+++ src/components/PrimeiroComponente.jsx
@@ -1,12 +1,37 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import axios from 'axios';
 
 export default class PrimeiroComponente extends Component {
+  state = {
+    user: {}
+  }
+
+  componentDidMount() {
+    axios.get("https://reqres.in/api/users")
+      .then(response => {
+        this.setState({ user: response.data.data[0] });
+      })
+  }
+
   render() {
     return(
       <View>
         <Text style={styles.titulo}>Meu primeiro componente está funcionando</Text>
         <Text style={styles.paragrafo}>Só mais um texto</Text>
+
+        <View>
+          <Image 
+            style={{
+              width: 200,
+              height: 200
+            }}
+            source={{ uri: this.state.user.avatar }}  
+          />
+          <Text style={styles.paragrafo}>{this.state.user.email}</Text>
+          <Text style={styles.paragrafo}>{this.state.user.id}</Text>
+          <Text style={styles.paragrafo}>{this.state.user.first_name} {this.state.user.last_name}</Text>
+        </View>
 
         <Button 
           title="ParOuImpar"
